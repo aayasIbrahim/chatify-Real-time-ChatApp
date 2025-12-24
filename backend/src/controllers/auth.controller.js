c9382e8d597ba46ae31995d1074e7f6c3fa90a34
Ayas Ibrahim <aayasabraham@gmail.com>
2025-12-24 17:57:29 +0600

    feat: implement login and logout functionality in auth controller and routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -67,3 +67,30 @@ export const signup = async (req, res) => {
     });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+    const isPassWordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPassWordCorrect)
+      return res.status(400).json({ message: "Invalid Credentials" });
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.error("Error in Log in controller :", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+export const logout = (_, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ message: "Logged Out successfully" });
+};
