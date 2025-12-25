9ceac1c389a108a14011fc8eb6e1508ba536b445
Ayas Ibrahim <aayasabraham@gmail.com>
2025-12-25 12:08:08 +0600

    feat: integrate Cloudinary for profile picture uploads and add update profile functionality

--- backend/lib/env.js
+++ backend/lib/env.js
@@ -8,5 +8,8 @@ export const ENV = {
   RESEND_API_KEY: process.env.RESEND_API_KEY,
   EMAIL_FROM: process.env.EMAIL_FROM,
   EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
-  CLIENT_URL:process.env.CLIENT_URL
+  CLIENT_URL: process.env.CLIENT_URL,
+  ClOUDINARY_CLOUD_NAME:process.env.ClOUDINARY_CLOUD_NAME,
+  ClOUDINARY_API_KEY:process.env.ClOUDINARY_API_KEY,
+  ClOUDINARY_API_SECRET:process.env.ClOUDINARY_API_SECRET,
 };
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import { generateToken } from "../../lib/utils.js";
 import bcrypt from "bcryptjs";
 import { sendWelcomeEmail } from "../email/emailHandler.js";
 import { ENV } from "../../lib/env.js";
+import cloudinary from "../../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -70,8 +71,8 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-  if(!email || !password) {
-    return res.status(400).json({message:"Email and Password required"})
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and Password required" });
   }
   try {
     const user = await User.findOne({ email });
@@ -96,4 +97,26 @@ export const login = async (req, res) => {
 export const logout = (_, res) => {
   res.cookie("jwt", "", { maxAge: 0 });
   res.status(200).json({ message: "Logged Out successfully" });
+};
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    if (!profilePic)
+      return res.status(400).json({ message: "Profile Pic is require" });
+    const userId = req.user._id;
+    const uploadResponse = await cloudinary.upload(profilePic);
+    const updateUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    );
+    res.status(200).json(updateUser);
+  } catch (error) {
+    console.error("Error in updateprofie function", error);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 };
