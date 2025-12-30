6471a02a770885ebbfbbe7675e9c1a590cf9e3dc
Ayas Ibrahim <aayasabraham@gmail.com>
2025-12-30 21:14:16 +0600

    feat:Implement Zustand to logInPage

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,18 @@
-import{create} from "zustand";
-export const useAuthStore=create(()=>({
-    login:()=>{
-        console.log("login")
-    },
-    isloading:false
-}))
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isCheckingAuth: true,
+ checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+      
+    } catch (error) {
+      console.log("Error in authCheck:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
