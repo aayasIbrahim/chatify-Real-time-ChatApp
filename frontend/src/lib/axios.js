e62f8e70eb413beb31428dacb6cf9202ea467b2a
Ayas Ibrahim <aayasabraham@gmail.com>
2026-01-02 18:06:05 +0600

    feat: implement ChatPage setUp

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,9 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "https://chatify-real-time-chatapp.onrender.com/api"
+      ? "https://chatify-real-time-chatapp.onrender.com/api/"
       : "/api",
   withCredentials: true,
 });
+
+ 
