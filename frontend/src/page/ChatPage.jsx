647d61b907ec338ce50cc608ab05fd660abcee20
Ayas Ibrahim <aayasabraham@gmail.com>
2026-01-05 19:49:05 +0600

    implement  chat-container

--- frontend/src/page/ChatPage.jsx
+++ frontend/src/page/ChatPage.jsx
@@ -1,16 +1,15 @@
 import { useChatStore } from "../store/useChatStore";
-
 import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
 import ProfileHeader from "../components/ProfileHeader";
 import ActiveTabSwitch from "../components/ActiveTabSwitch";
-import ChatsList from "../components/ChatList"
+import ChatsList from "../components/ChatList";
 import ContactList from "../components/ContactList";
 import ChatContainer from "../components/ChatContainer";
 import NoConversationPlaceholder from "../components/NoConversationPlaceholderNoConversationPlaceholder";
 
 function ChatPage() {
   const { activeTab, selectedUser } = useChatStore();
-
+ 
   return (
     <div className="relative w-full max-w-6xl h-[800px]">
       <BorderAnimatedContainer>
